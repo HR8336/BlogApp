a686ba9639c106b3ba92e5e719b6de8a16465dd8
harsh-zadafiya <zadafiyaharsh4@gmail.com>
2022-06-26 18:27:34 +0530

    ./

--- demo/src/component/Form.js
+++ demo/src/component/Form.js
@@ -1,101 +1,129 @@
-import React from 'react'
+import React from "react";
 import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
-import Select from 'react-select'
+import Select from "react-select";
 import { yupResolver } from "@hookform/resolvers/yup";
 import "./form.css";
 import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 
 const Form = () => {
-
   const defaultValue = {
-    name : '',
-    email: '',
-  }
-    const options = [
-        {  label: 'Health' , value: 'Health'},
-        { label: 'News', value: 'News'},
-        { label: 'Music' , value: 'Music' },
-       ];
-    const schema = yup.object().shape({
-        email: yup.string().email("Enter valid email").required("Please Enter Email"),
-        name: yup.string().max(32 , "name is not more then 32 char").required("please Enter name").min(4 , "name is no less then 4 char"),
-        category : yup.object().required()
-      }); 
-        const { register, handleSubmit, formState: { errors } , reset ,control , watch  } = useForm({
-          mode : "onBlur",
-          resolver: yupResolver(schema),
-          defaultValue
-        });
-        const onSubmitHandler = (data) => {
-          console.log(data)
-          reset()
-        };
-        return (
-            <> 
-            <div className='boxWrap'>
-            <form  onSubmit={handleSubmit(onSubmitHandler)}>
+    name: "",
+    email: "",
+  };
+  const options = [
+    { label: "Health", value: "Health" },
+    { label: "News", value: "News" },
+    { label: "Music", value: "Music" },
+  ];
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Enter valid email")
+      .required("Please Enter Email"),
+    name: yup
+      .string()
+      .max(32, "name is not more then 32 char")
+      .required("please Enter name")
+      .min(4, "name is no less then 4 char"),
+    category: yup.object().required(),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+    watch,
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+    defaultValue,
+  });
+  const onSubmitHandler = (data) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <>
+      <div className="boxWrap">
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <br />
+          <input
+            {...register("name", {
+              onBlur: () => console.log(watch("name")),
+            })}
+            placeholder="name"
+            type="text"
+          />
+          <p>{errors.name?.message}</p>
+          <br />
 
-            <br />  
-            <input
-              {...register("name" , {onBlur : () => console.log(watch('name')) })}
-              placeholder="name"
-              type="text"
-            />
-            <p>{errors.name?.message}</p>
-            <br />
-               
-            <input {...register("email" , {onBlur : () => console.log(watch('email'))}) } placeholder="email" type="email"/>
-            <p>{errors.email?.message}</p>
-            <br />
-  
-            <Controller
-                control={control}
-                name={"category"}
-                defaultValue = ''
-                   render={({ field }) => (
-                     <Select
-                     {...field }
-                     options={options}
-                     onBlur={() => {
-                      watch('category') &&  console.log(watch('category'));           
-              }}
-                     />
-            )}             
-             />
-            <p>{errors.category&& ("Please select atleast one")}</p>
-            <br/>
-            <div>
-                  <input name="ch1" type="checkbox" {...register('ch1' , {onBlur : () => console.log(watch('ch1'))})}/>
-                  <label>CH1</label>  
-                  <input name="ch2" type="checkbox" {...register('ch2' , {onBlur : () => console.log(watch('ch2'))})}/>
-                  <label>CH2</label>                   
-                  <p>{errors.acceptTerms?.message}</p>
-             </div>           
-             <Controller
-                name="calender"
-                control={control}
-                required
-                render={({ field }) => (
-                    <Datetime
-                        {...field}
-                        inputProps={{
-                            placeholder: "MM-DD-YYYY HH:mm",
-                        }}
-                viewMode="time"
-                onClose={() => {
-                  watch('calender') && console.log(watch('calender')._d);      
-                  }}
+          <input
+            {...register("email", {
+              onBlur: () => console.log(watch("email")),
+            })}
+            placeholder="email"
+            type="email"
+          />
+          <p>{errors.email?.message}</p>
+          <br />
+
+          <Controller
+            control={control}
+            name={"category"}
+            defaultValue=""
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={options}
+                onBlur={() => {
+                  watch("category") && console.log(watch("category"));
+                }}
               />
             )}
-        />
-        <br/>
+          />
+          <p>{errors.category && "Please select atleast one"}</p>
+          <br />
+          <div>
+            <input
+              name="ch1"
+              type="checkbox"
+              {...register("ch1", { onBlur: () => console.log(watch("ch1")) })}
+            />
+            <label>CH1</label>
+            <input
+              name="ch2"
+              type="checkbox"
+              {...register("ch2", { onBlur: () => console.log(watch("ch2")) })}
+            />
+            <label>CH2</label>
+            <p>{errors.acceptTerms?.message}</p>
+          </div>
+          <Controller
+            name="calender"
+            control={control}
+            required
+            render={({ field }) => (
+              <Datetime
+                {...field}
+                inputProps={{
+                  placeholder: "MM-DD-YYYY HH:mm",
+                }}
+                viewMode="time"
+                onClose={() => {
+                  watch("calender") && console.log(watch("calender")._d);
+                }}
+              />
+            )}
+          />
+          <br />
 
-            <button type="submit">Submit</button>
-            </form>
-            </div>
-            </>         
-        );      
-}
-export default Form
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default Form;
